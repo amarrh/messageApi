47e6a7fa89f74fe62c6ea2a30fc4acf9fa07c196
Amar Hodžić <66332497+amarrh@users.noreply.github.com>
2021-09-19 23:52:20 +0200

    Update settings.py

--- messageApi/settings.py
+++ messageApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#z8(p!4+oc5+cn*ppm0f=u52u)l&&ijxi56c$$u4&*%9s)09l('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
